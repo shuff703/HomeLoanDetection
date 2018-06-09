627bdbbbe41db573cb0fda2d234ade77c5c31c1a
Scott Huff <shuff703@gmail.com>
2018-06-08 22:42:57 -0400

    helmet

--- LoanDetection.R
+++ LoanDetection.R
@@ -50,7 +50,43 @@ sapply(dplyr::select_if(application_train, is.numeric), max, na.rm = TRUE)
 sapply(dplyr::select_if(application_train, is.factor), mode)
 print(dplyr::select_if(application_train, is.factor))
 
+#plots
+d <- density(application_train$AMT_INCOME_TOTAL)
+plot(d) 
+
 print(temp[1:300,])
+
+levels(bureau$CREDIT_ACTIVE)
+
+app_bur <- merge(application_train, bureau, by='SK_ID_CURR', how='left')
+
+app_bur_inner <- merge(application_train, bureau, by='SK_ID_CURR')
+
+#feature generation
+final_data <- data.frame(id=application_train$SK_ID_CURR)
+final_data$class <- application_train$TARGET
+final_data$credit_vs_income <- application_train$AMT_CREDIT/application_train$AMT_INCOME_TOTAL
+final_data$annuity_vs_income <- application_train$AMT_ANNUITY/application_train$AMT_INCOME_TOTAL
+final_data$price_vs_loan <- application_train$AMT_GOODS_PRICE/application_train$AMT_CREDIT
+
+#Generate feature for bad debt or sold credit report status
+bad_records <- filter(app_bur, app_bur$CREDIT_ACTIVE %in% c('Bad debt', 'Sold'))
+final_data$status_flag <- ifelse(final_data$id %in% bad_records$SK_ID_CURR, 1, 0)
+#5243 records esketit
+sum(final_data$status_flag == 1)
+
+#Credit Type - Cash, Car, etc.
+final_data$credit_type <- app_bur[match(unique(app_bur$SK_ID_BUREAU), app_bur$SK_ID_BUREAU),]
+
+#app_bur %>% group_by(SK_ID_CURR) %>% 
+  
+levels(bureau$CREDIT_TYPE)
+
+final_data <- app_bur %>%
+  group_by(SK) %>%
+  select('CREDIT_TYPE') %>%
+  slice(1) %>%
+  ungroup
 
 #simple nn
 if(!require('nnet')) install.packages('nnet')
@@ -58,9 +94,9 @@ library(nnet)
 
 ann <- nnet(TARGET~., MaxNWts = 10000, data=application_train, size = 30)
 
-ann_pred <- predict(ann, newdata = application_test, type='raw') 
+ann_pred <- predict(ann, newdata = application_test, type='raw')
 
 if(!require('caret')) install.packages('caret')
 library(caret)
 
-confusionMatrix(ann_pred,, positive=levels(test_data$gen_election.class[2]))
+confusionMatrix(ann_pred, , positive=levels(test_data$gen_election.class[2]))
